1ab40b8d9afca9e03d460d5800aae2bb8365510f
Morgan <m.garricks@lancaster.ac.uk>
2024-11-05 18:33:46 +0000

    improved website design

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+#morgan
+#Password123
+
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("meal", "status")
+    list_filter = ("status",)
+    search_fields = ("meal", "description")
+
+admin.site.register(Item, MenuItemAdmin)
